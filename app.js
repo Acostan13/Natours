16e761a41d45a01e368bab0d766aa0f91bc50a59
Alex Costan <acostan.con@orangetheory.com>
2020-09-24 14:20:16 -0400

    handling post requests

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const fs = require('fs')
 
 const app = express()
 
+// middleware => function that can modify the incoming request data
+// stand between the request and the response
+app.use(express.json())
+
 // Get request => Read
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -25,6 +29,7 @@ const tours = JSON.parse(
 )
 
 app.get('/api/v1/tours', (req, res) => {
+    // status 200: OK
     res.status(200).json({
         status: 'success',
         results: tours.length, // only relevant to use when you are getting/sending an array with multiple objects
@@ -32,6 +37,25 @@ app.get('/api/v1/tours', (req, res) => {
             tours // If data has same name as property (tours), you can exclude the value
         },
     })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    // console.log(req.body)
+    const newId = tours[tours.length - 1].id + 1
+    const newTour = Object.assign({id: newId}, req.body)
+
+    tours.push(newTour)
+    // status 201: Created
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+
+    // res.send('Done') // Data must always be sent in order to finish the req/res cycle
 })
 
 const port = 3000
