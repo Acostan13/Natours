3e2315e5faa6bbfa26dd13bf4270287f02353707
Alex Costan <acostan.con@orangetheory.com>
2020-09-25 10:56:51 -0400

    handling patch requests

--- app.js
+++ app.js
@@ -85,6 +85,23 @@ app.post('/api/v1/tours', (req, res) => {
     // res.send('Done') // Data must always be sent in order to finish the req/res cycle
 })
 
+// Updating tour data
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Updated tour here...>'
+        }
+    })
+})
+
 const port = 3000
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
