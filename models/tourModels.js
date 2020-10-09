55ce8287858cf983b5f6399b1df5881bb07ea38a
Alex Costan <acostan.con@orangetheory.com>
2020-10-09 16:25:30 -0400

    document middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,67 +1,85 @@
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const slugify = require('slugify')
 
-dotenv.config({ path: './config.env' })
+const tourSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'A tour must have name'], // validator
+            unique: true,
+            true: true
+        },
+        slug: String,
+        duration: {
+            type: Number,
+            required: [true, 'A tour must have duration']
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'A tour must have a maximum group size']
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'A tour must have a difficulty']
+        },
+        ratingsAverage: {
+            type: Number,
+            default: 4.5
+        },
+        ratingsQuantity: {
+            type: Number,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: [true, 'A tour must have price']
+        },
+        priceDiscount: Number,
+        summary: {
+            type: String,
+            trim: true,
+            required: [true, 'A tour must have a summary']
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'A tour must have an image cover']
+        },
+        images: [String],
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            select: false
+        },
+        startDates: [Date]
+    },
+    {
+        toJSON: { virtuals: true }
+    }
+)
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have name'], // validator
-        unique: true,
-        true: true
-    },
-    duration: {
-        type: Number,
-        required: [true, 'A tour must have duration']
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'A tour must have a maximum group size']
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'A tour must have a difficulty']
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 4.5
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have price']
-    },
-    priceDiscount: Number,
-    summary: {
-        type: String,
-        trim: true,
-        required: [true, 'A tour must have a summary']
-    },
-    description: {
-        type: String,
-        trim: true
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'A tour must have an image cover']
-    },
-    images: [String],
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        select: false
-    },
-    startDates: [Date]
-}, {
-    toJSON: { virtuals: true }
-})
-
-tourSchema.virtual('durationWeeks').get(function() {
+tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7
 })
+
+// Document Middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true })
+    next()
+})
+
+// tourSchema.pre('save', function (next) {
+//     console.log('Will save document')
+//     next()
+// })
+
+// tourSchema.post('save', function (doc, next) {
+//     console.log(doc)
+//     next()
+// })
 
 // Always capitalize variables for Models or Schema objects
 const Tour = mongoose.model('Tour', tourSchema)
