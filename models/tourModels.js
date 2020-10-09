a72c32a97d75c30a794e8b0ee1b403b4a7aa121e
Alex Costan <acostan.con@orangetheory.com>
2020-10-09 16:58:54 -0400

    query middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -54,7 +54,11 @@ const tourSchema = new mongoose.Schema(
             default: Date.now(),
             select: false
         },
-        startDates: [Date]
+        startDates: [Date],
+        secretTour: {
+            type: Boolean,
+            default: false
+        }
     },
     {
         toJSON: { virtuals: true }
@@ -80,6 +84,21 @@ tourSchema.pre('save', function (next) {
 //     console.log(doc)
 //     next()
 // })
+
+// Query middlwware
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } })
+
+    this.start = Date.now()
+    next()
+})
+
+tourSchema.post(/^find/, function(docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`)
+    console.log(docs)
+    next()
+})
 
 // Always capitalize variables for Models or Schema objects
 const Tour = mongoose.model('Tour', tourSchema)
