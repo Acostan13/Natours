7ce7073142731d56e94b773d6ff90b039645fd63
Alex Costan <acostan.con@orangetheory.com>
2020-10-10 09:24:52 -0400

    validating data with built-in validators

--- models/tourModels.js
+++ models/tourModels.js
@@ -7,7 +7,15 @@ const tourSchema = new mongoose.Schema(
             type: String,
             required: [true, 'A tour must have name'], // validator
             unique: true,
-            true: true
+            trim: true,
+            maxlength: [
+                40,
+                'A tour name must have less than or equal to 40 characters'
+            ],
+            minlength: [
+                10,
+                'A tour name must have more than or equal to 10 characters'
+            ]
         },
         slug: String,
         duration: {
@@ -20,11 +28,17 @@ const tourSchema = new mongoose.Schema(
         },
         difficulty: {
             type: String,
-            required: [true, 'A tour must have a difficulty']
+            required: [true, 'A tour must have a difficulty'],
+            enum: {
+                values: ['easy', 'medium', 'difficult'],
+                message: 'Difficulty must be either easy, medium, or difficult'
+            }
         },
         ratingsAverage: {
             type: Number,
-            default: 4.5
+            default: 4.5,
+            min: [1, 'Rating must be above 1.0'],
+            max: [5, 'Rating must be below 5.0']
         },
         ratingsQuantity: {
             type: Number,
@@ -94,7 +108,7 @@ tourSchema.pre(/^find/, function (next) {
     next()
 })
 
-tourSchema.post(/^find/, function(docs, next) {
+tourSchema.post(/^find/, function (docs, next) {
     console.log(`Query took ${Date.now() - this.start} milliseconds!`)
     next()
 })
