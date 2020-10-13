68e00e871a81b2528aed804c189c972d19d2f5d2
Alex Costan <acostan.con@orangetheory.com>
2020-10-13 15:15:49 -0400

    adding 404 Not Found Errors

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,9 @@
 const Tour = require('../models/tourModels')
 const APIFeatures = require('../utils/apiFeatures')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
 
-exports.aliasTopTours = async (req, res, next) => {
+exports.aliasTopTaurs = async (req, res, next) => {
     req.query.limit = '5'
     req.query.sort = '-ratingsAverage,price'
     req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
@@ -33,6 +34,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id)
     // Tour.findOne({ _id: req.params.id})
+
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -53,11 +59,16 @@ exports.createTour = catchAsync(async (req, res, next) => {
     })
 })
 
-exports.updateTour = catchAsync(async (req, res) => {
+exports.updateTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
         runValidators: true
     })
+
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
+
     res.status(200).json({
         status: 'success',
         data: {
@@ -66,8 +77,13 @@ exports.updateTour = catchAsync(async (req, res) => {
     })
 })
 
-exports.deleteTour = catchAsync(async (req, res) => {
-    await Tour.findByIdAndDelete(req.params.id)
+exports.deleteTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findByIdAndDelete(req.params.id)
+
+    if (!tour) {
+        return next(new AppError('No tour found with that ID', 404))
+    }
+
     res.status(204).json({
         // Status code 204: No content
         status: 'success',
