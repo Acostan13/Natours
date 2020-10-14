2d79990946d24e8a582bd88784de6cc6ade858d5
Alex Costan <acostan.con@orangetheory.com>
2020-10-14 13:33:14 -0400

    handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,15 +1,22 @@
 const AppError = require('../utils/appError')
 
-const handleCastErrorDB = (err, res) => {
+const handleCastErrorDB = (err) => {
     const message = `Invalid ${err.path}: ${err.value}.`
     return new AppError(message, 400)
 }
 
-const handleDuplicateFieldsDB = (err, res) => {
+const handleDuplicateFieldsDB = (err) => {
     const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+    console.log(value)
 
     const message = `Duplicate field value: ${value}. Please use another value!`
-    return new AppError(message, 404)
+    return new AppError(message, 400)
+}
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map((el) => el.message)
+
+    const message = `Invalid input data. ${errors.join('. ')}`
+    return new AppError(message, 400)
 }
 
 const sendErrorDev = (err, res) => {
@@ -43,7 +50,7 @@ const sendErrorProd = (err, res) => {
 }
 
 module.exports = (err, req, res, next) => {
-    // console.log(err.stack)
+    // console.log(err.stack);
 
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
@@ -51,14 +58,12 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res)
     } else if (process.env.NODE_ENV === 'production') {
-        // eslint-disable-next-line node/no-unsupported-features/es-syntax
         let error = { ...err }
-        if (error.name === 'CastError') {
-            error = handleCastErrorDB(error)
-        }
-        if (error.name === 11000) {
-            error = handleDuplicateFieldsDB(error)
-        }
+
+        if (error.name === 'CastError') error = handleCastErrorDB(error)
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+        if (error.name === 'ValidationError')
+            error = handleValidationErrorDB(error)
 
         sendErrorProd(error, res)
     }
