43354f8204a9266413e25f87ee71fdb78f718df2
Alex Costan <acostan.con@orangetheory.com>
2020-10-20 09:03:48 -0400

    protecting the tour routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 
-const signToken = id => {
+const signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWS_EXPIRES_IN
     })
@@ -28,16 +28,16 @@ exports.signup = catchAsync(async (req, res, next) => {
     })
 })
 
-exports.login = catchAsync( async (req, res, next) => {
+exports.login = catchAsync(async (req, res, next) => {
     const { email, password } = req.body
 
     // 1) Check if email and password exist
-    if (!email || !password ) {
+    if (!email || !password) {
         return next(new AppError('Please provide email and password!', 400))
     }
 
     // 2) Check if the user exits && password is correct
-    const user = await User.findOne({email: email}).select('+password')
+    const user = await User.findOne({ email: email }).select('+password')
 
     if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401))
@@ -49,4 +49,27 @@ exports.login = catchAsync( async (req, res, next) => {
         status: 'success',
         token
     })
+})
+
+exports.protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check if it's there
+    let token
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1]
+    }
+    console.log(token)
+
+    if (!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401))
+    }
+    // 2) Verification token
+
+    // 3) Check if user still exists
+
+    // 4) Check if user changed password after the token was issued
+
+    next()
 })
