ba4473adb829c4a3950ecfcc9ef4283e8893961b
Alex Costan <acostan.con@orangetheory.com>
2020-10-21 14:17:35 -0400

    sending jwt via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,12 +8,25 @@ const sendEmail = require('../utils/email')
 
 const signToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWS_EXPIRES_IN
+        expiresIn: process.env.JWT_EXPIRES_IN
     })
 }
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
+
+    res.cookie('jwt', token, cookieOptions)
+
+    // Remove password from the output
+    user.password = undefined
 
     res.status(statusCode).json({
         status: 'success',
