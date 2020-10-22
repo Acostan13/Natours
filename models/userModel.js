654a4db2364e81268fd8ec88b61f6511c8c934aa
Alex Costan <acostan.con@orangetheory.com>
2020-10-22 14:44:28 -0400

    implementing password reset functionality with reset tokens

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
@@ -47,7 +48,9 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords do not match'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 })
 
 userSchema.pre('save', async function (next) {
@@ -75,11 +78,26 @@ userSchema.methods.changedPasswordAfter = function (JWTTimeStamp) {
             this.passwordChangedAt.getTime() / 1000,
             10
         )
-        
+
         return JWTTimeStamp < changedTimeStamp
     }
     // False means NOT changed
     return false
+}
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex')
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex')
+
+    console.log({resetToken}, this.passwordResetToken)
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000
+
+    return resetToken
 }
 
 const User = mongoose.model('User', userSchema)
