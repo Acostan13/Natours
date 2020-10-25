19b757bed0bcd530d82eb6ad8c2101b6a2b48abc
Alex Costan <acostan.con@orangetheory.com>
2020-10-25 12:23:05 -0400

    modelling locations with geospatial data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -21,7 +21,7 @@ mongoose
     .then(() => console.log('DB connection established'))
 
 // Reads JSON files
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 // Importing data into mongodb
 const importData = async () => {
