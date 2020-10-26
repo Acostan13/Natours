c9a229b6fa119d81686a3e827f4b87a8a2078470
Alex Costan <acostan.con@orangetheory.com>
2020-10-26 11:30:16 -0400

    populating tour guides

--- models/tourModels.js
+++ models/tourModels.js
@@ -155,12 +155,21 @@ tourSchema.pre(/^find/, function (next) {
     next()
 })
 
+tourSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'guides',
+        select: '-__v -passwordChangedAt'
+    })
+
+    next()
+})
+
 tourSchema.post(/^find/, function (docs, next) {
     console.log(`Query took ${Date.now() - this.start} milliseconds!`)
     next()
 })
 
-// Aggregatioin middleware
+// Aggregation middleware
 tourSchema.pre('aggregate', function (next) {
     this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
 
