500240aeea7003156bc57fd88bdb7462337364f5
Alex Costan <acostan.con@orangetheory.com>
2020-10-28 11:21:45 -0400

    importing review and user data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const Tour = require("../../models/tourModels")
+const Tour = require('../../models/tourModel')
+const Review = require('../../models/reviewModel')
+const User = require('../../models/userModel')
 
 dotenv.config({ path: './config.env' })
 
@@ -16,17 +18,21 @@ mongoose
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
-        useUnifiedTopology: true 
+        useUnifiedTopology: true
     })
     .then(() => console.log('DB connection established'))
 
 // Reads JSON files
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'))
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'))
 
 // Importing data into mongodb
 const importData = async () => {
     try {
         await Tour.create(tours)
+        await User.create(users, { validateBeforeSave: false })
+        await Review.create(reviews)
         console.log('Data successfully loaded!')
     } catch (err) {
         console.log(err)
@@ -38,6 +44,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany()
+        await User.deleteMany()
+        await Review.deleteMany()
         console.log('Data successfully loaded!')
     } catch (err) {
         console.log(err)
