161c7c883473e9fef43dccd061c26e4db860670d
Alex Costan <acostan.con@orangetheory.com>
2020-10-30 16:36:29 -0400

    setting up the project structure

---
+++ routes/viewRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const viewsCotroller = require('../controllers/viewsController')
+
+const router = express.Router()
+
+router.get('/', viewsCotroller.getOverview)
+router.get('/tour', viewsCotroller.getTour)
+
+module.exports = router
