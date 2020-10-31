6bb826f8b8305955e7eb574baf9bfbf92da5fae8
Alex Costan <acostan.con@orangetheory.com>
2020-10-31 09:21:35 -0400

    building the tour page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const viewsCotroller = require('../controllers/viewsController')
 const router = express.Router()
 
 router.get('/', viewsCotroller.getOverview)
-router.get('/tour', viewsCotroller.getTour)
+router.get('/tour/:slug', viewsCotroller.getTour)
 
 module.exports = router
